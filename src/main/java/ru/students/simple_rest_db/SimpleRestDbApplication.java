3c271fb0be8804f6a4381932fcc1c7a589fe5e1e
dimarick <dimon.ksk@gmail.com>
2023-09-12 08:23:52 +0500

    Initial commit

---
+++ src/main/java/ru/students/simple_rest_db/SimpleRestDbApplication.java
@@ -0,0 +1,13 @@
+package ru.students.simple_rest_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRestDbApplication.class, args);
+    }
+
+}
